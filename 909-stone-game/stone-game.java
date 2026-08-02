c9842c7440d8a6b22f87d5e7c7832c0353f1c528
Suparna-Kanti-Manna <suparnakanti33@gmail.com>
2026-08-03 01:44:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (88.64%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean stoneGame(int[] nums) {
+        // int n = nums.length;
+        // int i=0, j=n-1, sum1 = 0, sum2 = 0;
+        // while(n!=0){
+        //     if(n%2==0){
+        //         if(nums[i]>=nums[j]){
+        //             sum1 += nums[i];
+        //             i++;
+        //             n--;
+        //         }else{
+        //             sum1 += nums[j];
+        //             j--;
+        //             n--;
+        //         }
+        //     }else{
+        //         if(nums[i]>=nums[j]){
+        //             sum2 += nums[i];
+        //             i++;
+        //             n--;
+        //         }else{
+        //             sum2 += nums[j];
+        //             j--;
+        //             n--;
+        //         }
+        //     }
+            
+        // }
+        // if(sum1 > sum2){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
+        return true;
+    }
+}
