5284f2e9c3dba3e828f2449db6bccc2b11b2c0ce
Suparna-Kanti-Manna <suparnakanti33@gmail.com>
2026-08-24 13:06:32 +0530

    Time: 106 ms (60.76%) | Memory: 87.3 MB (81.26%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+
+        // Convert stones into prefix sums.
+        for (int i = 1; i < n; ++i) {
+            stones[i] += stones[i - 1];
+        }
+
+        int dp = stones[n - 1];
+
+        for (int i = n - 2; i >= 1; --i) {
+            dp = max(dp, stones[i] - dp);
+        }
+
+        return dp;
+    }
+};
